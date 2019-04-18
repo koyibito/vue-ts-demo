0461d90dde17b1e0221291461dcf44b35377bb12
yishikejinew <jianfeng.ge@1rock.cn>
2019-04-18 18:17:08 +0800

    add

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -2,17 +2,23 @@
     <div>
         <div class="content" v-if="visible">
             <p>我是一个产品详情组件</p>
+            <p v-on:click="callParent">点我调用父组件的方法</p>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
 
     @Component
     export default class ProductDetail extends Vue {
         @Prop({ default: false })
         private visible!: boolean;
+
+        @Emit()
+        private callParent(): void {
+            console.log('我是产品详情组件内容');
+        }
     }
 </script>
 
--- src/page/Home.vue
+++ src/page/Home.vue
@@ -6,7 +6,7 @@
         </router-link>
         <div>
             <p v-on:click="switchProductDetail">点击【显示-隐藏】ProductDetail</p>
-            <product-detail :visible="isShowProductDetail"></product-detail>
+            <product-detail :visible="isShowProductDetail" v-on:callParent="alertInfo"></product-detail>
         </div>
     </div>
 </template>
@@ -26,6 +26,10 @@
 
         private switchProductDetail(): void {
             this.isShowProductDetail = !this.isShowProductDetail;
+        }
+
+        private alertInfo(): void {
+            console.log('子组件调用父组件方法');
         }
     }
 </script>
